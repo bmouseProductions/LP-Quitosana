0bc94c2c1611fba6bd3f7bbf30775f92fa0a4ddf
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-07-24 13:42:48 -0300

    final

--- src/components/button/Index.tsx
+++ src/components/button/Index.tsx
@@ -7,7 +7,7 @@ interface botaoProps{
 
 export default function Botao( {children}:botaoProps){
     return (
-        <a href="#form" className="w-full md:max-w-[22.8rem]"> 
+        <a href="https://api.whatsapp.com/send/?phone=554792034473&text&type=phone_number&app_absent=0" className="w-full md:max-w-[22.8rem]"> 
             <button className="w-full  py-5 rounded font-bold text-black text-[26px] bg-[#fff] shadow-[0px_0px_20px_#ff7e00] hover:bg-[#f55303] hover:text-white hover:shadow-[0px_0px_20px_#000] transition-all duration-200">
                 {children}
             </button>
--- src/components/whatsapp/Index.tsx
+++ src/components/whatsapp/Index.tsx
@@ -4,7 +4,7 @@ import imgWpp from '../../assets/whatsapp.png'
 export default function Whatsapp(){
     return (
         <div className='fixed left-5 bottom-5 z-50 animate-bounce'>
-            <a href="" target='_blank' className=' '>
+            <a href="https://api.whatsapp.com/send/?phone=554792034473&text&type=phone_number&app_absent=0" target='_blank' className=' '>
                 <img src={imgWpp} alt='icone whatsapp' className='w-[54px] h-[54px] ' />
             </a>
         </div>
